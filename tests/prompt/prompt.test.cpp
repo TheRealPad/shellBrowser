b2c30b1ef7277f3017b8bf414783522bbd75f1b4
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-10-03 22:39:53 -0700

    [Mod] chaneg unit test to test errors

--- tests/prompt/prompt.test.cpp
+++ tests/prompt/prompt.test.cpp
@@ -2,16 +2,9 @@
 #include <cassert>
 
 int main() {
-    // Test 1 : Vérifiez si l'addition de 2 et 3 est égale à 5
-    assert(addition(2, 3) == 5);
-
-    // Test 2 : Vérifiez si l'addition de -1 et 1 est égale à 0
-    assert(addition(-1, 1) == 0);
-
-    // Test 3 : Vérifiez si l'addition de 0 et 0 est égale à 0
-    assert(addition(0, 0) == 0);
-
+    int a = 5;
+    int b = 6;
+    assert(a == b && "a n'est pas égal à b");
     std::cout << "Tous les tests ont réussi !" << std::endl;
-
     return 0;
 }
