9cba2653d05aff1001d4674d452041c85c566d79
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-10-03 22:34:14 -0700

    [Rem] remove boost from dependencies

--- tests/prompt/prompt.test.cpp
+++ tests/prompt/prompt.test.cpp
@@ -1,6 +1,17 @@
-#define BOOST_TEST_MODULE VTKTests
-#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <cassert>
 
-BOOST_AUTO_TEST_CASE(test_one_equals_one) {
-    BOOST_CHECK_EQUAL(1, 1);
+int main() {
+    // Test 1 : Vérifiez si l'addition de 2 et 3 est égale à 5
+    assert(addition(2, 3) == 5);
+
+    // Test 2 : Vérifiez si l'addition de -1 et 1 est égale à 0
+    assert(addition(-1, 1) == 0);
+
+    // Test 3 : Vérifiez si l'addition de 0 et 0 est égale à 0
+    assert(addition(0, 0) == 0);
+
+    std::cout << "Tous les tests ont réussi !" << std::endl;
+
+    return 0;
 }
